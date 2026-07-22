69bbe7cadd4539ca289b90a1e3ace115c553b6a4
yousefMohsen <officialyousefmohsen@gmail.com>
2026-07-22 20:41:52 +0300

    Add Tabs component infrastructure

---
+++ docs/.vitepress/theme/components/tabs/PluginTabsTab.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed, inject, nextTick, watch } from 'vue'
+import { useIsPrint } from './composables'
+import { useTabsRenderAll, useTabsSingleState } from './state'
+
+defineOptions({ name: 'PluginTabsTab' })
+
+const props = defineProps<{
+  anchor: string
+  label: string
+}>()
+
+const { selected, uid } = useTabsSingleState()
+const isPrint = useIsPrint()
+const renderAll = useTabsRenderAll()
+const isActive = computed(() =>
+  selected.value === props.label || Boolean(isPrint.value) || renderAll
+)
+
+const panelId = computed(() => `panel-${props.anchor}-${uid}`)
+const buttonId = computed(() => `tab-${props.anchor}-${uid}`)
+</script>
+
+<template>
+  <div
+    v-if="isActive"
+    :id="panelId"
+    class="plugin-tabs--content"
+    role="tabpanel"
+    tabindex="0"
+    :data-tab-anchor="anchor"
+    :aria-labelledby="buttonId"
+    :data-is-print="isPrint || undefined"
+  >
+    <slot />
+  </div>
+</template>
